d8cc2ab340387c1e0afa8a15e7efa30dfe7625bc
DishaSingh07 <dishumau@gmail.com>
2024-07-27 16:37:48 +0530

    supersnd

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import { Routes } from "react-router-dom";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 import { Route } from "react-router-dom";
-import Home from "./pages/Home"
-import Cart from "./pages/Cart"
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import SuprSendInbox from "@suprsend/react-inbox";
+import "react-toastify/dist/ReactToastify.css"; // needed for toast notifications,
 
 const App = () => {
-  return (<div>
-        <div className="bg-slate-900">
-          <Navbar/>
-        </div>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/cart" element={<Cart/>} />
-        </Routes>
-  </div>)
+  return (
+    <div>
+      <div className="bg-slate-900">
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+      <SuprSendInbox
+        workspaceKey={process.env.REACT_APP_SUPRSEND_WORKSPACE_KEY}
+        subscriberId={process.env.REACT_APP_SUPRSEND_SUBSCRIBER_ID}
+        distinctId={process.env.REACT_APP_SUPRSEND_DISTINCT_ID}
+      />
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,18 +7,21 @@ import "./index.css";
 import { store } from "./redux/Store";
 import { ToastContainer } from "react-toastify";
 import { Toaster } from "react-hot-toast";
+import SuprSendInbox from '@suprsend/react-inbox';
+import 'react-toastify/dist/ReactToastify.css'; // needed for toast notifications, can be ignored if hideToast=true
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <BrowserRouter>
-        <Provider store={store}>
-          <App />   
-          <Toaster/>  
-        </Provider>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+      <Toaster />
+    </Provider>
+  </BrowserRouter>
 
 
 
-  
+
 );
