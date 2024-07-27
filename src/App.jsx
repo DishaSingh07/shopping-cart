840607933c738a7953283b4e7b1418cd669778ec
DishaSingh07 <dishumau@gmail.com>
2024-07-27 17:22:01 +0530

    vars

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { Routes } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import { Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import SuprSendInbox from "@suprsend/react-inbox";
-import "react-toastify/dist/ReactToastify.css"; // needed for toast notifications,
+import "react-toastify/dist/ReactToastify.css"; // needed for toast notifications
+import { v4 as uuidv4 } from "uuid";
 
 const App = () => {
+  const workspaceKey = process.env.REACT_APP_SUPRSEND_WORKSPACE_KEY;
+  const distinctId = uuidv4(); // Generate a unique identifier
+  const subscriberId = uuidv4(); // Generate a unique identifier
+
   return (
     <div>
       <div className="bg-slate-900">
@@ -16,7 +21,11 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
-      
+      <SuprSendInbox
+        workspaceKey={workspaceKey}
+        subscriberId={subscriberId}
+        distinctId={distinctId}
+      />
     </div>
   );
 };
